023c49932b4dd88a7d24f82d7f88ab9cda4a1738
徐淳 <bluexuchun@gmail.com>
2019-01-14 21:27:36 +0800

    add usercenter
    add usercenter

--- pages/usercenter/usercenter.vue
+++ pages/usercenter/usercenter.vue
@@ -9,11 +9,33 @@
 </template>
 
 <script>
+	import is from 'is'
+	import Utils from 'utils'
+	import api from '@/utils/api'
+	import mixin from '@/utils/mixin'
 	export default {
+		mixins:[mixin],
+		computed: {
+		    userInfo() {
+		        return this.$store.state.app.userInfo;
+		    }
+		},
 		data() {
 			return {
 				
 			};
+		},
+		methods:{
+			async init(){
+				console.log('123')
+				console.log(this.userInfo)
+			}
+		},
+		onShow(){
+			let _this = this
+			if(!_this.shouldLogin){
+				_this.init()
+			}
 		}
 	}
 </script>
